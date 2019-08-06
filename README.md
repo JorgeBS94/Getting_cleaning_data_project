010de78422e654d1f042139b7ec12586323f7960
JorgeBS94 <jorge.bretones.santamarina@gmail.com>
2019-08-06 11:36:21 +0200

    ReadMe update, final run_analysis.R

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Getting_cleaning_data_project
-This is a repository for the Coursera course on Getting and Cleaning data by the Johns Hopkins University
+
+Throughout this project, we have obtained and cleaned data from accelerometers from a Samsung Galaxy S device. The goal is to provide a tidy dataset that can be further analysed. This repository contains all the files employed, which are the following:
+
+**run_analysis.R** : this file contains the main steps used to download and transform the data up to the obtention of a tidy dataset. 
+
+
+**CodeBook.Rmd**: it describes the variables, the data and every transformation we performed in order to clean up the data.
+
+
+**tidy_data.txt**: it is a text file containing the tidy dataset generated as an output of the "run_analysis.R" file.
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,10 @@
 ################### SCRIPT TO ANALYSE THE DATA OF THE GETTING AND CLEANING DATA COURSE PROJECT
 
-##### Download the data and unzip it
+##### Set the working directory
+
+setwd("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project")
+
+##### Download the data folder and unzip it
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile = "S5_dataset.zip",method = "curl")
@@ -8,67 +12,87 @@ zipF <- "C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEAN
 outDir<- "C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset"
 unzip(zipF,exdir=outDir)
 
-##### Upload the training and the test datasets
+##### Upload the training and the test datasets and their subject and label informations
 
 #Training dataset
 training <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/train/X_train.txt",row.names = NULL)
 training_labels <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/train/y_train.txt",row.names = NULL)
+training_subjects <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/train/subject_train.txt")
 
 #Test dataset
 testing <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/test/X_test.txt",row.names = NULL)
 testing_labels <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/test/y_test.txt",row.names = NULL)
+testing_subjects <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/test/subject_test.txt")
 
-##### Upload and use the feature vector and the activity and subject names
-
-#Feature vector
+# Upload the vector of features
 features <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/features.txt",row.names = NULL)
 features <- as.character(features[,2])
 
-#Rename the variables in our data set according the the feature names
+#Rename the variables in our data according to feature names
 names(training) <- features
 names(testing) <- features
 
-#Subjects
-subject_train <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/train/subject_train.txt")
-subject_test <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/test/subject_test.txt")
+#Add the activity labels and the subject info to both the training and the testing data
+training$label <- training_labels$V1
+testing$label <- testing_labels$V1
+training$subject <- training_subjects$V1
+testing$subject <- testing_subjects$V1
 
-#We add the subjects as a column to the dataframes
-training$subject <- as.factor(subject_train$V1)
-testing$subject <- as.factor(subject_test$V1)
+################################
 
-#Activity names
+# 1. Merging the training and the test sets to create one data set (we pile them up together)
+
+my_data <- rbind(testing,training)
+
+################################
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+#Use regular expressions to extract only the column names containing means or stds
+good_cols<- grep("mean|std|subject|label",colnames(my_data))
+my_data <- my_data[,good_cols]
+
+################################
+
+# 3: Add the labels to the training and testing sets, changing the names according to activity names
+
+#Read the activity names
 activity <- read.table("C:/Users/jorge_000/Desktop/ONLINE_COURSES/DATA_SCIENCE_COURSERA/3_CLEANING_DATA/Getting_cleaning_data_project/S5_dataset/UCI HAR Dataset/activity_labels.txt",row.names = NULL)
 
-#We add the labels to the training and testing sets, changing the names according to activity names
-training$labels <- factor(training_labels$V1,labels = as.character(activity$V2))
-testing$labels <- factor(testing_labels$V1,labels = as.character(activity$V2))
+#We replace the label numbers by the corresponding activity name in our data frame
+my_data$activity <- factor(my_data$label, levels = activity$V1, labels = activity$V2)
 
-#### Merging the training and the test sets to create one data set (we do it by common column names)
-my_data <- rbind(testing,training)
-dim(my_data)
+#We eliminate the label column, as we no longer need it
+my_data$label <- NULL
 
-#### Extracts only the measurements on the mean and standard deviation for each measurement.
+################################
 
-#We use regular expressions to identify which columns in our dataset contain the words mean and sd:
+# 4: replace variable names by informative values
+var_names <- colnames(my_data)
 
-good_cols<- grepl("\\bmean()\\b|\\bstd()\\b",colnames(my_data))
-new_data <- my_data[,good_cols]
-colnames(new_data)
-                 
-#### Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+#We can apply a series of changes using regular expressions in order to make the names more 
+#informative. We will use the magrittr package to avoid writing too much
+
+library(magrittr)
+var_names %<>%
+    gsub("^t","Time_domain",.) %>%
+    gsub("^f","Frequency_domain",.) %>%
+    gsub("Acc","Accelerometer",.) %>%
+    gsub("Gyro","Gyroscope",.) %>%
+    gsub("Mag","Magnitude",.) %>%
+    gsub("std","Standard_deviation",.)
+
+#We apply our variable name changes back to the original data
+names(my_data) <- var_names
+
+################################
+
+# 5: Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 library(plyr)
 
 #Now we use the ddply function to subset the dataframe and summarize it according to the mean function:
-tidy_data <- ddply(new_data,.(labels,subject),colwise(mean,na.rm = TRUE))
+tidy_data <- ddply(my_data,.(activity,subject),colwise(mean,na.rm = TRUE))
 
-#Note that ddply eliminates the duplicated columns automatically, so we see that the number of dimensions has changed
-dim(tidy_data)
-
-#Another way to do it is with the aggregate function. However, in this case duplicated columns are not handled for us, 
-#so we must get them rid of them manually:
-
-tidy_data2 <- aggregate(new_data, by = list(new_data$subject, new_data$labels),mean)
-#We use the unique () function of the dplyr package to subset the unique columns from our dataframe:
-tidy_data2 <- tidy_data2[,unique(colnames(new_data))]
-#We see that the dimensions are equal to those above
-dim(tidy_data2)
+##### Save tidy dataset as a text file
+write.table(tidy_data,"tidy_data.txt",row.names = FALSE)
